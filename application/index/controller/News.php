378f3ae529b18acfacec99c06c10cd58e988e614
yang <feiye0425@163.com>
2020-10-17 09:21:14 +0800

    1

--- application/index/controller/News.php
+++ application/index/controller/News.php
@@ -14,26 +14,15 @@ class News extends Controller
 /*      $news_tb = db('news')->where(['pid'=>'6','title'=>'123'])->select();*/
 /*      dump($news_tb);*/
 
-    $news_tb = db('news')->where(['pid'=>6])->select();
-
-
-
-
-
+    $news_tb = db('news')->where(['pid'=>$id])->select();
 
        $news_find = db('news')->find(['pid'=>$id]);
         if (empty($news_find)) {
             $this->error("该文章不存在或已删除！",'index/index/index');
         }
 
-
-
-
-
+           
         $pid = $news_find['pid'];
-
-
-   
 
         $newscate_model = model('\app\admin\model\Newsfenlei');
 
